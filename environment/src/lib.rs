fdd00f26fe7d3a5192a941b6e0e299a7bba79c93
BoredApe8461 <letesinkelegesse@gmail.com>
2023-12-12 10:07:32 +0100

    add uniques extension | unimplemented

--- environment/src/lib.rs
+++ environment/src/lib.rs
@@ -1,4 +1,20 @@
+// This file is part of RegionX.
+//
+// RegionX is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// RegionX is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with RegionX.  If not, see <https://www.gnu.org/licenses/>.
+
 use ink::env::{DefaultEnvironment, Environment};
+use uniques_extension::UniquesExtension;
 
 /// Our custom environment diverges from the `DefaultEnvironment` in the event topics
 /// limit.
@@ -16,5 +32,5 @@ impl Environment for ExtendedEnvironment {
     type Timestamp = <DefaultEnvironment as Environment>::Timestamp;
 
     // TODO: add uniques chain extension
-    type ChainExtension = <DefaultEnvironment as Environment>::ChainExtension;
+    type ChainExtension = UniquesExtension;
 }

172755b6f36f1279ff6863e7d9cd6ae50831494a
BoredApe8461 <letesinkelegesse@gmail.com>
2024-01-26 19:08:15 +0100

    extrinsic calls work

--- tests/market/list.test.ts
+++ tests/market/list.test.ts
@@ -6,10 +6,13 @@ import Market_Factory from '../../types/constructors/coretime_market';
 import XcRegions from '../../types/contracts/xc_regions';
 import Market from '../../types/contracts/coretime_market';
 import chaiAsPromised from 'chai-as-promised';
+import { CoreMask, Region, RegionId, RegionRecord } from 'coretime-utils';
 
 use(chaiAsPromised);
 
-const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+const REGION_COLLECTION_ID = 42;
+
+const wsProvider = new WsProvider('ws://127.0.0.1:9920');
 // Create a keyring instance
 const keyring = new Keyring({ type: 'sr25519' });
 
@@ -34,7 +37,94 @@ describe('Coretime market listing', () => {
     market = new Market((await marketFactory.new(xcRegions.address, 0)).address, alice, api);
   });
 
-  it('Listing works', () => {
-    
+  it('Listing works', async () => {
+    const regionId: RegionId = {
+      begin: 30,
+      core: 0,
+      mask: CoreMask.completeMask(),
+    };
+    const regionRecord: RegionRecord = {
+      end: 60,
+      owner: alice.address,
+      paid: null,
+    };
+    const region = new Region(regionId, regionRecord);
+
+    await createRegionCollection(api, alice);
+    await mintRegion(api, alice, region);
+    await approveTransfer(api, alice, region, xcRegions.address);
   });
 });
+
+async function createRegionCollection(
+  contractsApi: ApiPromise,
+  caller: KeyringPair,
+): Promise<void> {
+  console.log(`Creating the region collection`);
+
+  const createCollectionCall = contractsApi.tx.uniques.create(REGION_COLLECTION_ID, caller.address);
+
+  const callTx = async (resolve: () => void, reject: ({ reason }) => void) => {
+    const unsub = await createCollectionCall.signAndSend(caller, ({ status, events }) => {
+      if (status.isInBlock) {
+        unsub();
+        events.forEach(({ event: { method, section } }) => {
+          if (section == 'system' && method == 'ExtrinsicFailed')
+            reject({ reason: 'Creating collection failed' });
+        });
+        resolve();
+      }
+    });
+  };
+
+  return new Promise(callTx);
+}
+
+async function mintRegion(api: ApiPromise, caller: KeyringPair, region: Region): Promise<void> {
+  console.log(`Minting a region`);
+
+  const rawRegionId = region.getEncodedRegionId(api);
+  const mintCall = api.tx.uniques.mint(REGION_COLLECTION_ID, rawRegionId, caller.address);
+
+  const callTx = async (resolve: () => void, reject: ({ reason }) => void) => {
+    const unsub = await mintCall.signAndSend(caller, ({ status, events }) => {
+      if (status.isInBlock) {
+        unsub();
+        events.forEach(({ event: { method, section } }) => {
+          if (section == 'system' && method == 'ExtrinsicFailed')
+            reject({ reason: 'Minting failed' });
+        });
+        resolve();
+      }
+    });
+  };
+
+  return new Promise(callTx);
+}
+
+async function approveTransfer(
+  api: ApiPromise,
+  caller: KeyringPair,
+  region: Region,
+  delegate: string,
+): Promise<void> {
+  console.log(`Approving region to ${delegate}`);
+
+  const rawRegionId = region.getEncodedRegionId(api);
+  const approveCall = api.tx.uniques.approveTransfer(REGION_COLLECTION_ID, rawRegionId, delegate);
+
+  const callTx = async (resolve: () => void, reject: ({ reason }) => void) => {
+    const unsub = await approveCall.signAndSend(caller, ({ status, events }) => {
+      if (status.isInBlock) {
+        unsub();
+        events.forEach(({ event: { method, section } }) => {
+          if (section == 'system' && method == 'ExtrinsicFailed')
+            reject({ reason: 'Approving region failed' });
+        });
+        resolve();
+      }
+    });
+  };
+
+  return new Promise(callTx);
+}

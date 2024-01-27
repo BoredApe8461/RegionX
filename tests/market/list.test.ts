32ac57ed24060ba3450e3292f21b8512690751ca
BoredApe8461 <letesinkelegesse@gmail.com>
2024-01-27 10:36:13 +0100

    listing fully tested

--- tests/market/list.test.ts
+++ tests/market/list.test.ts
@@ -7,10 +7,12 @@ import XcRegions from '../../types/contracts/xc_regions';
 import Market from '../../types/contracts/coretime_market';
 import chaiAsPromised from 'chai-as-promised';
 import { CoreMask, Id, Region, RegionId, RegionRecord } from 'coretime-utils';
+import { MarketErrorBuilder, PSP34ErrorBuilder } from '../../types/types-returns/coretime_market';
 
 use(chaiAsPromised);
 
 const REGION_COLLECTION_ID = 42;
+const LISTING_DEPOIST = 100;
 
 const wsProvider = new WsProvider('ws://127.0.0.1:9944');
 // Create a keyring instance
@@ -34,7 +36,15 @@ describe('Coretime market listing', () => {
     xcRegions = new XcRegions((await xcRegionsFactory.new()).address, alice, api);
 
     const marketFactory = new Market_Factory(api, alice);
-    market = new Market((await marketFactory.new(xcRegions.address, 0)).address, alice, api);
+    market = new Market(
+      (await marketFactory.new(xcRegions.address, LISTING_DEPOIST)).address,
+      alice,
+      api,
+    );
+
+    if (!(await api.query.uniques.class(REGION_COLLECTION_ID)).toHuman()) {
+      await createRegionCollection(api, bob);
+    }
   });
 
   it('Listing works', async () => {
@@ -50,7 +60,6 @@ describe('Coretime market listing', () => {
     };
     const region = new Region(regionId, regionRecord);
 
-    await createRegionCollection(api, bob);
     await mintRegion(api, bob, region);
     await approveTransfer(api, bob, region, xcRegions.address);
 
@@ -60,12 +69,100 @@ describe('Coretime market listing', () => {
     await xcRegions.withSigner(bob).tx.approve(market.address, id, true);
 
     const bitPrice = 50;
-    await market.withSigner(bob).tx.listRegion(id, bitPrice, bob.address, 0, 0);
+    await market
+      .withSigner(bob)
+      .tx.listRegion(id, bitPrice, bob.address, 0, 0, { value: LISTING_DEPOIST });
 
     await expectOnSale(market, id, bob, bitPrice);
     expect((await market.query.regionPrice(id)).value.unwrap().ok.toNumber()).to.be.equal(
       bitPrice * 80,
     );
+    expect((await xcRegions.query.ownerOf(id)).value.unwrap()).to.deep.equal(market.address);
+  });
+
+  it('Listing requires listing deposit', async () => {
+    const regionId: RegionId = {
+      begin: 30,
+      core: 1,
+      mask: CoreMask.completeMask(),
+    };
+    const regionRecord: RegionRecord = {
+      end: 60,
+      owner: bob.address,
+      paid: null,
+    };
+    const region = new Region(regionId, regionRecord);
+
+    await mintRegion(api, bob, region);
+    await approveTransfer(api, bob, region, xcRegions.address);
+
+    await initRegion(api, xcRegions, bob, region);
+
+    const id: any = api.createType('Id', { U128: region.getEncodedRegionId(api) });
+    await xcRegions.withSigner(bob).tx.approve(market.address, id, true);
+
+    const bitPrice = 50;
+    const result = market.withSigner(bob).query.listRegion(id, bitPrice, bob.address, 0, 0);
+    expect((await result).value.unwrap().err).to.deep.equal(MarketErrorBuilder.MissingDeposit());
+  });
+
+  it('Listing requires region to be approved to the market', async () => {
+    const regionId: RegionId = {
+      begin: 30,
+      core: 2,
+      mask: CoreMask.completeMask(),
+    };
+    const regionRecord: RegionRecord = {
+      end: 60,
+      owner: bob.address,
+      paid: null,
+    };
+    const region = new Region(regionId, regionRecord);
+
+    await mintRegion(api, bob, region);
+    await approveTransfer(api, bob, region, xcRegions.address);
+
+    await initRegion(api, xcRegions, bob, region);
+
+    const id: any = api.createType('Id', { U128: region.getEncodedRegionId(api) });
+
+    const bitPrice = 50;
+    const result = await market
+      .withSigner(bob)
+      .query.listRegion(id, bitPrice, bob.address, 0, 0, { value: LISTING_DEPOIST });
+    expect(result.value.unwrap().err).to.deep.equal(
+      MarketErrorBuilder.XcRegionsPsp34Error(PSP34ErrorBuilder.NotApproved()),
+    );
+  });
+
+  it('Listing expired region fails', async () => {
+    const regionId: RegionId = {
+      begin: 0,
+      core: 3,
+      mask: CoreMask.completeMask(),
+    };
+    const regionRecord: RegionRecord = {
+      end: 1,
+      owner: bob.address,
+      paid: null,
+    };
+    const region = new Region(regionId, regionRecord);
+
+    await mintRegion(api, bob, region);
+    await approveTransfer(api, bob, region, xcRegions.address);
+
+    await initRegion(api, xcRegions, bob, region);
+
+    const id: any = api.createType('Id', { U128: region.getEncodedRegionId(api) });
+    await xcRegions.withSigner(bob).tx.approve(market.address, id, true);
+
+    setTimeout(async () => {
+      const bitPrice = 50;
+      const result = await market
+        .withSigner(bob)
+        .query.listRegion(id, bitPrice, bob.address, 0, 0, { value: LISTING_DEPOIST });
+      expect(result.value.unwrap().err).to.deep.equal(MarketErrorBuilder.RegionExpired());
+    }, 6000);
   });
 });
 
